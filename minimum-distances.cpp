2a0eb554670610fe0ef9f7002bfe6d9f2bd0b57a
--global <--global>
2024-07-08 10:34:57 +0700

    minimum distances hackerank problem

---
+++ minimum-distances.cpp
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/minimum-distances/problem
+#include <bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define db double
+#define ldb long double
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n, a, ans = INT_MAX;
+  cin >> n;
+
+  map<int, int> mp;
+  for (int i = 1; i <= n; i++) {
+    cin >> a;
+    if (mp[a])
+      ans = min(ans, i - mp[a]);
+    mp[a] = i;
+  }
+
+  if (ans == INT_MAX)
+    cout << -1;
+  else
+    cout << ans;
+
+  return 0;
+}
